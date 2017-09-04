272422de1bf667200ec9f323e258f229b258a224
mokejp <mokejp@users.noreply.github.com>
2017-09-04 12:39:49 +0900

    Add group/room member profile API

--- linebot/get_profile_test.go
+++ linebot/get_profile_test.go
@@ -146,3 +146,251 @@ func BenchmarkGetProfile(b *testing.B) {
 		client.GetProfile("U0047556f2e40dba2456887320ba7c76d").Do()
 	}
 }
+
+func TestGetGroupMemberProfile(t *testing.T) {
+	type want struct {
+		URLPath     string
+		RequestBody []byte
+		Response    *UserProfileResponse
+		Error       error
+	}
+	var testCases = []struct {
+		GroupID      string
+		UserID       string
+		ResponseCode int
+		Response     []byte
+		Want         want
+	}{
+		{
+			GroupID:      "cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+			UserID:       "U0047556f2e40dba2456887320ba7c76d",
+			ResponseCode: 200,
+			Response:     []byte(`{"userId":"U0047556f2e40dba2456887320ba7c76d","displayName":"BOT API","pictureUrl":"http://dl.profile.line.naver.jp/abcdefghijklmn","statusMessage":"Hello, LINE!"}`),
+			Want: want{
+				URLPath:     fmt.Sprintf(APIEndpointGetGroupMemberProfile, "cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "U0047556f2e40dba2456887320ba7c76d"),
+				RequestBody: []byte(""),
+				Response: &UserProfileResponse{
+					UserID:        "U0047556f2e40dba2456887320ba7c76d",
+					DisplayName:   "BOT API",
+					PictureURL:    "http://dl.profile.line.naver.jp/abcdefghijklmn",
+					StatusMessage: "Hello, LINE!",
+				},
+			},
+		},
+		{
+			// Internal server error
+			GroupID:      "cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+			UserID:       "U0047556f2e40dba2456887320ba7c76d",
+			ResponseCode: 500,
+			Response:     []byte("500 Internal server error"),
+			Want: want{
+				URLPath:     fmt.Sprintf(APIEndpointGetGroupMemberProfile, "cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "U0047556f2e40dba2456887320ba7c76d"),
+				RequestBody: []byte(""),
+				Error: &APIError{
+					Code: 500,
+				},
+			},
+		},
+	}
+
+	var currentTestIdx int
+	server := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		tc := testCases[currentTestIdx]
+		if r.Method != http.MethodGet {
+			t.Errorf("Method %s; want %s", r.Method, http.MethodGet)
+		}
+		if r.URL.Path != tc.Want.URLPath {
+			t.Errorf("URLPath %s; want %s", r.URL.Path, tc.Want.URLPath)
+		}
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !reflect.DeepEqual(body, tc.Want.RequestBody) {
+			t.Errorf("RequestBody %s; want %s", body, tc.Want.RequestBody)
+		}
+		w.WriteHeader(tc.ResponseCode)
+		w.Write(tc.Response)
+	}))
+	defer server.Close()
+	client, err := mockClient(server)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i, tc := range testCases {
+		currentTestIdx = i
+		res, err := client.GetGroupMemberProfile(tc.GroupID, tc.UserID).Do()
+		if tc.Want.Error != nil {
+			if !reflect.DeepEqual(err, tc.Want.Error) {
+				t.Errorf("Error %d %q; want %q", i, err, tc.Want.Error)
+			}
+		} else {
+			if err != nil {
+				t.Error(err)
+			}
+		}
+		if !reflect.DeepEqual(res, tc.Want.Response) {
+			t.Errorf("Response %d %q; want %q", i, res, tc.Want.Response)
+		}
+	}
+}
+
+func TestGetGroupMemberProfileWithContext(t *testing.T) {
+	server := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		time.Sleep(10 * time.Millisecond)
+		w.Write([]byte("{}"))
+	}))
+	defer server.Close()
+	client, err := mockClient(server)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Millisecond)
+	defer cancel()
+	_, err = client.GetGroupMemberProfile("cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "U0047556f2e40dba2456887320ba7c76d").WithContext(ctx).Do()
+	if err != context.DeadlineExceeded {
+		t.Errorf("err %v; want %v", err, context.DeadlineExceeded)
+	}
+}
+
+func BenchmarkGetGroupMemberProfile(b *testing.B) {
+	server := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		w.Write([]byte(`{"userId":"U","displayName":"A","pictureUrl":"http://","statusMessage":"B"}`))
+	}))
+	defer server.Close()
+	client, err := mockClient(server)
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		client.GetGroupMemberProfile("cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "U0047556f2e40dba2456887320ba7c76d").Do()
+	}
+}
+
+func TestGetRoomMemberProfile(t *testing.T) {
+	type want struct {
+		URLPath     string
+		RequestBody []byte
+		Response    *UserProfileResponse
+		Error       error
+	}
+	var testCases = []struct {
+		RoomID       string
+		UserID       string
+		ResponseCode int
+		Response     []byte
+		Want         want
+	}{
+		{
+			RoomID:       "cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+			UserID:       "U0047556f2e40dba2456887320ba7c76d",
+			ResponseCode: 200,
+			Response:     []byte(`{"userId":"U0047556f2e40dba2456887320ba7c76d","displayName":"BOT API","pictureUrl":"http://dl.profile.line.naver.jp/abcdefghijklmn","statusMessage":"Hello, LINE!"}`),
+			Want: want{
+				URLPath:     fmt.Sprintf(APIEndpointGetRoomMemberProfile, "cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "U0047556f2e40dba2456887320ba7c76d"),
+				RequestBody: []byte(""),
+				Response: &UserProfileResponse{
+					UserID:        "U0047556f2e40dba2456887320ba7c76d",
+					DisplayName:   "BOT API",
+					PictureURL:    "http://dl.profile.line.naver.jp/abcdefghijklmn",
+					StatusMessage: "Hello, LINE!",
+				},
+			},
+		},
+		{
+			// Internal server error
+			RoomID:       "cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+			UserID:       "U0047556f2e40dba2456887320ba7c76d",
+			ResponseCode: 500,
+			Response:     []byte("500 Internal server error"),
+			Want: want{
+				URLPath:     fmt.Sprintf(APIEndpointGetRoomMemberProfile, "cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "U0047556f2e40dba2456887320ba7c76d"),
+				RequestBody: []byte(""),
+				Error: &APIError{
+					Code: 500,
+				},
+			},
+		},
+	}
+
+	var currentTestIdx int
+	server := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		tc := testCases[currentTestIdx]
+		if r.Method != http.MethodGet {
+			t.Errorf("Method %s; want %s", r.Method, http.MethodGet)
+		}
+		if r.URL.Path != tc.Want.URLPath {
+			t.Errorf("URLPath %s; want %s", r.URL.Path, tc.Want.URLPath)
+		}
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !reflect.DeepEqual(body, tc.Want.RequestBody) {
+			t.Errorf("RequestBody %s; want %s", body, tc.Want.RequestBody)
+		}
+		w.WriteHeader(tc.ResponseCode)
+		w.Write(tc.Response)
+	}))
+	defer server.Close()
+	client, err := mockClient(server)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i, tc := range testCases {
+		currentTestIdx = i
+		res, err := client.GetRoomMemberProfile(tc.RoomID, tc.UserID).Do()
+		if tc.Want.Error != nil {
+			if !reflect.DeepEqual(err, tc.Want.Error) {
+				t.Errorf("Error %d %q; want %q", i, err, tc.Want.Error)
+			}
+		} else {
+			if err != nil {
+				t.Error(err)
+			}
+		}
+		if !reflect.DeepEqual(res, tc.Want.Response) {
+			t.Errorf("Response %d %q; want %q", i, res, tc.Want.Response)
+		}
+	}
+}
+
+func TestGetRoomMemberProfileWithContext(t *testing.T) {
+	server := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		time.Sleep(10 * time.Millisecond)
+		w.Write([]byte("{}"))
+	}))
+	defer server.Close()
+	client, err := mockClient(server)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Millisecond)
+	defer cancel()
+	_, err = client.GetRoomMemberProfile("cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "U0047556f2e40dba2456887320ba7c76d").WithContext(ctx).Do()
+	if err != context.DeadlineExceeded {
+		t.Errorf("err %v; want %v", err, context.DeadlineExceeded)
+	}
+}
+
+func BenchmarkGetRoomMemberProfile(b *testing.B) {
+	server := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		w.Write([]byte(`{"userId":"U","displayName":"A","pictureUrl":"http://","statusMessage":"B"}`))
+	}))
+	defer server.Close()
+	client, err := mockClient(server)
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		client.GetRoomMemberProfile("cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "U0047556f2e40dba2456887320ba7c76d").Do()
+	}
+}
