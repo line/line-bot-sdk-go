09bb493161444dad0cd35db892b8ae6333a56686
makoto <makoto@makoto-mac-9881.local>
2019-11-15 17:29:14 +0900

    Fixed declaration of FlexContainerType

--- linebot/flex.go
+++ linebot/flex.go
@@ -17,7 +17,7 @@ package linebot
 import "encoding/json"
 
 // FlexContainerType type
-type FlexContainerType = string // alias type
+type FlexContainerType string // alias type
 
 // IntPtr is a helper function for using *int values
 func IntPtr(v int) *int {
