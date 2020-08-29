a26a6319ee4da32ea86b3668bb63f51dfc3d74df
raahii <piyo56.net@gmail.com>
2020-08-29 16:00:21 +0900

    Update channel access token v2.1 apis
    News: https://developers.line.biz/en/news/2020/06/22/new-channel-access-token-endpoint-and-deprecation/

--- linebot/response.go
+++ linebot/response.go
@@ -250,11 +250,12 @@ type AccessTokenResponse struct {
 	AccessToken string `json:"access_token"`
 	ExpiresIn   int64  `json:"expires_in"`
 	TokenType   string `json:"token_type"`
+	KeyID       string `json:"key_id"`
 }
 
 // AccessTokensResponse type
 type AccessTokensResponse struct {
-	AccessTokens []string `json:"access_tokens"`
+	KeyIDs []string `json:"kids"`
 }
 
 func checkResponse(res *http.Response) error {
