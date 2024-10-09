312bd304686623751fadfd035d882f6f151085b1
Moca-Love <raimu_hero@yahoo.co.jp>
2024-10-09 13:24:25 +0900

    add QuotedMessageID

--- linebot/message.go
+++ linebot/message.go
@@ -52,9 +52,10 @@ type SendingMessage interface {
 // TextMessage type
 // Deprecated: Use OpenAPI based classes instead.
 type TextMessage struct {
-	ID     string
-	Text   string
-	Emojis []*Emoji
+	ID              string
+	Text            string
+	Emojis          []*Emoji
+	QuotedMessageID string
 
 	quickReplyItems *QuickReplyItems
 	sender          *Sender
@@ -66,11 +67,12 @@ type TextMessage struct {
 // MarshalJSON method of TextMessage
 func (m *TextMessage) MarshalJSON() ([]byte, error) {
 	return json.Marshal(&struct {
-		Type       MessageType      `json:"type"`
-		Text       string           `json:"text"`
-		QuickReply *QuickReplyItems `json:"quickReply,omitempty"`
-		Sender     *Sender          `json:"sender,omitempty"`
-		Emojis     []*Emoji         `json:"emojis,omitempty"`
+		Type            MessageType      `json:"type"`
+		Text            string           `json:"text"`
+		QuickReply      *QuickReplyItems `json:"quickReply,omitempty"`
+		Sender          *Sender          `json:"sender,omitempty"`
+		Emojis          []*Emoji         `json:"emojis,omitempty"`
+		QuotedMessageID string           `json:"quotedMessageId,omitempty"`
 	}{
 		Type:       m.messageType,
 		Text:       m.Text,
@@ -317,6 +319,7 @@ type StickerMessage struct {
 	StickerResourceType StickerResourceType
 	Keywords            []string
 	Text                string
+	QuotedMessageID     string
 
 	quickReplyItems *QuickReplyItems
 	sender          *Sender
@@ -334,6 +337,7 @@ func (m *StickerMessage) MarshalJSON() ([]byte, error) {
 		QuickReply          *QuickReplyItems    `json:"quickReply,omitempty"`
 		Sender              *Sender             `json:"sender,omitempty"`
 		Text                string              `json:"text,omitempty"`
+		QuotedMessageID     string              `json:"quotedMessageId,omitempty"`
 	}{
 		Type:                m.messageType,
 		PackageID:           m.PackageID,
