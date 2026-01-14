a27e39be4db1579d0aa44f15abfa26e951a8526a
Euchi Yuta <yeuchi@lycorp.co.jp>
2026-01-14 16:00:25 +0900

    Delete ununsed code to generate code

--- generator/src/main/java/line/bot/generator/pebble/PebbleTemplateAdapter.java
+++ generator/src/main/java/line/bot/generator/pebble/PebbleTemplateAdapter.java
@@ -19,7 +19,6 @@ public class PebbleTemplateAdapter extends AbstractTemplatingEngineAdapter {
         .cacheActive(false)
         .newLineTrimming(false)
         .loader(new DelegatingLoader(Arrays.asList(
-            new FileLoader(),
             new ClasspathLoader()
         )))
         .autoEscaping(false)
